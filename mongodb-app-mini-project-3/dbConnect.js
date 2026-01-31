518ed9e0fad0b98ee074d1cc61630b57a4184443
amijshogan <amijsanghvi@gmail.com>
2026-01-31 12:25:31 -0800

    dbConnect.js
    Created my dbConnect.js file, added in some initial code as a starting point, & then imported it into my server.js sheet.

---
+++ mongodb-app-mini-project-3/dbConnect.js
@@ -0,0 +1,22 @@
+// terminal has caught a lot of problems & I can see some of them, too. so will be checking through this code to make sure it's fine.
+
+'use strict';
+const Mongoose = require('mongoose');
+// if the connection fails, try 127.0.0.1 instead of localhost below
+const uri = process.env.DB_URI ||
+"mongodb://localhost/myFirstDatabase";
+
+// Connect to MongoDB
+Mongoose.connect(uri)
+.then(() => console.log('MongoDB Connected'))
+.catch(error => console.log('MongoDB Error:
+'+error.message));
+
+// Get the default connection
+const db = Mongoose.connection;
+
+// Bind connection to error event (to get notification of connection errors)
+db.on("error", console.error.bind(console, "MongoDB
+connection error:"));
+
+exports.Mongoose = Mongoose;
--- mongodb-app-mini-project-3/server.js
+++ mongodb-app-mini-project-3/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+let dbConnect = require("./dbConnect");
 
 // parse requests of content-type - application/json
 app.use(express.json());
