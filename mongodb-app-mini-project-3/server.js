17a87f92b56e79f8949712ea86c597c200e4e087
amijshogan <amijsanghvi@gmail.com>
2026-01-31 18:42:13 -0800

    Continued server.js setup
    Continued building an elementary form of my server.js sheet.

--- mongodb-app-mini-project-3/server.js
+++ mongodb-app-mini-project-3/server.js
@@ -1,3 +1,39 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+
+const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+/**
+ * Asynchronously connects to MongoDB using Mongoose.
+ * @returns (Promise<void>)
+ */
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("process.env.MONGO_URI");
+    console.log("✅MongoDB Connected");
+  } catch (error) {
+    console.error("❌Database connection error:", error);
+    // Exit process with failure (1)
+    process.exit(1);
+  }
+};
+
+/**
+ * Starts the Express server after connecting to the database
+ * @returns void
+ */
+const startServer = async () => {
+  await connectDB();
+
+  const PORT = process.env.PORT;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+startServer();
