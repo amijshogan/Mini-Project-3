258c87a1c50d1f9ab5a20d17b98e7b2087ec2887
amijshogan <amijsanghvi@gmail.com>
2026-01-31 19:11:20 -0800

    Import Router & Initiate socket.io
    Imported bookRouter.js from my routes folder, and  then set up my initial initiation for my socket.io server. Also mounted my router via bookRoutes.js.

--- mongodb-app-mini-project-3/server.js
+++ mongodb-app-mini-project-3/server.js
@@ -4,14 +4,18 @@ const http = require("http");
 const { Server } = require("socket.io");
 require("dotenv").config();
 
-const app = express(); // not sure if this is the correct location for this line atm
-
 // Import the Router
 const { bookRouter } = require("../routes/bookRoutes.js"); // not sure if this is the correct location for this line atm
+const app = express();
+
+// Init Socket.io on the HTTP server
+const io = new Server(server);
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+//Mount the Router (Mount the User Routes)
+app.use("/api/books", bookRoutes); // need to check if this path is correct
 /**
  * Asynchronously connects to MongoDB using Mongoose.
  * @returns (Promise<void>)
