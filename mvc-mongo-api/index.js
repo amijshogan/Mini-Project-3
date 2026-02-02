1ba1f9f905909216edc4b89b5b09b280a0179050
amijshogan <amijsanghvi@gmail.com>
2026-02-02 11:01:53 -0800

    External api
    Added external API to index.js

--- mvc-mongo-api/index.js
+++ mvc-mongo-api/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const axios = require("axios");
 const http = require("http");
+const Book = require("./models/Book"); // Import your Mongoose model
 
 // Import the router
 const { bookRouter } = require("./routes/bookRoutes");
@@ -31,6 +33,8 @@ const connectDB = async () => {
     console.error("😔 Database connection error:", error);
     // Exit process with failure (1)
     process.exit(1);
+    const response = await axios.get(googleapis.com / books / v1);
+    const externalData = response.data; // The array of data from the API
   }
 };
 
