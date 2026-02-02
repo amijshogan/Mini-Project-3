4727648d210d5c4d013d5b7984866b041d76db31
amijshogan <amijsanghvi@gmail.com>
2026-02-02 08:04:14 -0800

    Added update & delete functions
    Created my updateBook & deleteBook functions inside the bookController sheet.

--- mvc-mongo-api/controllers/bookController.js
+++ mvc-mongo-api/controllers/bookController.js
@@ -7,6 +7,7 @@ const { Book } = require("..models/Book");
  * @returns {Promise<void>}
  */
 
+// functionality for finding book/text
 const getBooks = async (req, res) => {
   try {
     const books = await Book.find();
@@ -22,6 +23,7 @@ const getBooks = async (req, res) => {
  * @param {Object} res - Express response object
  * @returns {Promise<void>}
  */
+// functionality for creating book/text
 const createBook = async (req, res) => {
   try {
     const { title, author, year, text, email, createdBy } = req.body;
@@ -40,8 +42,49 @@ const createBook = async (req, res) => {
   }
 };
 
+/**
+ * Update a book by ID.
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @returns {Promise<void>}
+ */
+const updateBook = async (req, res) => {
+  try {
+    const { id } = req.params; // object that will hold all the url parameters -- /books/#
+    const updates = req.body;
+    const book = await Book.findByIdAndUpdate(id, updates, { new: true });
+    if (!book) {
+      return res.status(404).json({ success: false, error: "Book not found" });
+    }
+    res.status(200).json({ success: true, data: book });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+};
+
+/**
+ * Delete a book by ID. :(
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @returns {Promise<void>}
+ */
+const deleteBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findByIdAndDelete(id);
+    if (!book) {
+      return res.status(404).json({ success: false, error: "Book not found" });
+    }
+    res.status(200).json({ success: true, message: book }); // deleted book/book deleted
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
+
 // Exports
 module.exports = {
   getBooks,
   createBook,
+  updateBook,
+  deleteBook,
 };
