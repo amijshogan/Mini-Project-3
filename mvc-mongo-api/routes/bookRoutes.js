51efe2433f9dcd937bf21b4132b408dd346e9ee3
amijshogan <amijsanghvi@gmail.com>
2026-02-01 17:09:13 -0800

    Update bookRoutes.js
    Gave the program instructions on what it should be ~getting (book/text info from the user), and what it should be ~returning (existing book/text data/entries).

--- mvc-mongo-api/routes/bookRoutes.js
+++ mvc-mongo-api/routes/bookRoutes.js
@@ -2,3 +2,7 @@ const express = require("express");
 const { getBooks, createBook } = require("../controllers/bookController");
 
 const router = express.Router();
+
+// Map HTTP methods to Controller function
+router.get("/, getBooks");
+router.post("/", createBooks);
