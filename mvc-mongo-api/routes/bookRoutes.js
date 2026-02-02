e002bc4d74cdd339f5e6343e48bd66e8eda8d15e
amijshogan <amijsanghvi@gmail.com>
2026-02-02 08:11:13 -0800

    Update bookRoutes (update/delete)
    Created put & patch routes for updating a book + a route for deleting a book.

--- mvc-mongo-api/routes/bookRoutes.js
+++ mvc-mongo-api/routes/bookRoutes.js
@@ -1,11 +1,24 @@
 const express = require("express");
-const { getBooks, createBook } = require("../controllers/bookController");
+const {
+  getBooks,
+  createBook,
+  updateBook,
+  deleteBook,
+} = require("../controllers/bookController");
 
 const router = express.Router();
 
 // Map HTTP methods to Controller function
+// /api/books
 router.get("/", getBooks);
 router.post("/", createBook);
+
+// /api/books/:id
+// The ':id' is a placeholder for the actual Book ID
+router.put("/:id", updateBook); // put is when you are putting in ALL new data, for each category
+router.patch("/:id", updateBook); // patch means fixing only what needs to be fixed (so not everything like with put)
+// both are ways/protocols for updating data
+router.delete(":/id", deleteBook);
 
 // Exports
 module.exports = {
